8d48c7fdeec7332af2344f08592bf5563bdfd573
ElegantMindClub <elegantmindclub@gmail.com>
2024-05-15 17:35:35 -0700

    succesfful synchronous movement
    extremely fast
    
    Co-Authored-By: Rohan Adwankar <39285979+RohanAdwankar@users.noreply.github.com>

--- SerialStepperCoordination.ino
+++ SerialStepperCoordination.ino
@@ -19,17 +19,37 @@ int zpos;
 AccelStepper sx(1,xpul,xdir);
 AccelStepper sy(1,ypul,ydir);
 AccelStepper sz(1,zpul,zdir);
-//MultiStepper Control;
+MultiStepper StepperControl;
+long gotoposition[3];
 
 void setup()
 {
   Serial.begin(115200);
   delay(1000);
   Serial.println("setup()");
-  sx.setMaxSpeed(50000000);
-  sy.setMaxSpeed(50000000);
-  sz.setMaxSpeed(50000000);
-  sx.setAcceleration(1000);
+  // sx.setMaxSpeed(50000000);
+  // sy.setMaxSpeed(50000000);
+  // sz.setMaxSpeed(50000000);
+  // //sx.setAcceleration(1000);
+  sx.setMaxSpeed(3000);
+  sx.setSpeed(200);
+  sy.setMaxSpeed(3000);
+  sy.setSpeed(200);
+  sz.setMaxSpeed(3000);
+  sz.setSpeed(200);
+
+  StepperControl.addStepper(sx);
+  StepperControl.addStepper(sy);
+  StepperControl.addStepper(sz);
+  sx.setCurrentPosition(0);
+  sy.setCurrentPosition(0);
+  sz.setCurrentPosition(0);
+  gotoposition[0] = 100;
+  gotoposition[1] = 120;
+  gotoposition[2] = 130;
+  StepperControl.moveTo(gotoposition);
+  StepperControl.runSpeedToPosition();
+  delay(200);
 }
 
 void loop()
@@ -37,15 +57,15 @@ void loop()
   Serial.println("loop("+String(loopCounter)+")");
   int distance = 100;
   int speed = 100;
-  sx.setSpeed(speed);
-  sx.moveTo(distance);
-  sx.runSpeedToPosition();
-  sy.setSpeed(speed);
-  sy.moveTo(distance);
-  sy.runSpeedToPosition();
-  sz.setSpeed(speed);
-  sz.moveTo(distance);
-  sz.runSpeedToPosition();
+  // sx.setSpeed(speed);
+  // sx.moveTo(distance);
+  // sx.runSpeedToPosition();
+  // sy.setSpeed(speed);
+  // sy.moveTo(distance);
+  // sy.runSpeedToPosition();
+  // sz.setSpeed(speed);
+  // sz.moveTo(distance);
+  // sz.runSpeedToPosition();
   delay(1) ;
   loopCounter++;
   if(loopCounter>=32767){
