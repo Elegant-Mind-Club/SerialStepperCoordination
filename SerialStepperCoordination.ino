7de660acf51b91202eaa0f91087bd70a1be2452c
ElegantMindClub <elegantmindclub@gmail.com>
2024-05-15 17:15:24 -0700

    only x moves, no doubling effect
    Co-Authored-By: Rohan Adwankar <39285979+RohanAdwankar@users.noreply.github.com>

--- SerialStepperCoordination.ino
+++ SerialStepperCoordination.ino
@@ -1,4 +1,7 @@
 #include <AccelStepper.h>
+#include <MultiStepper.h>
+
+
 #define xpul 24
 #define xdir 30
 
@@ -16,17 +19,37 @@ int zpos;
 AccelStepper sx(1,xpul,xdir);
 AccelStepper sy(1,ypul,ydir);
 AccelStepper sz(1,zpul,zdir);
+//MultiStepper Control;
 
 void setup()
 {
   Serial.begin(115200);
   delay(1000);
   Serial.println("setup()");
+  sx.setMaxSpeed(50000000);
+  sy.setMaxSpeed(50000000);
+  sz.setMaxSpeed(50000000);
+  sx.setAcceleration(1000);
 }
 
 void loop()
 {
   Serial.println("loop("+String(loopCounter)+")");
+  int distance = 100;
+  int speed = 100;
+  sx.setSpeed(speed);
+  sx.moveTo(distance);
+  sx.runSpeedToPosition();
+  sy.setSpeed(speed);
+  sy.moveTo(distance);
+  sy.runSpeedToPosition();
+  sz.setSpeed(speed);
+  sz.moveTo(distance);
+  sz.runSpeedToPosition();
   delay(1) ;
   loopCounter++;
+  if(loopCounter>=32767){
+    Serial.println("Max loopCounter reached!");
+    loopCounter = 0;
+  }
 }
